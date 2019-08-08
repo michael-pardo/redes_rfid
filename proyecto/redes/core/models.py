d7c54007104b5cd535b90e35f9e963d29526ac38
Michael <michael.stiver.pardo@gmail.com>
2019-08-08 09:55:59 -0500

    Modificaciòn models

--- proyecto/redes/core/models.py
+++ proyecto/redes/core/models.py
@@ -58,6 +58,20 @@ class Auditoria(models.Model):
 
         super(Auditoria, self).save(*args, **kwargs)
 
+class Faccultad(Auditoria):
+    facultad = models.CharField(
+        max_length=20,
+        help_text="Facultad a la que pertenece..",
+        unique=True
+    )
+
+    def __str__(self):
+        return '{}'.format(self.user.get_full_name())
+
+    class Meta:
+        verbose_name = "Facultad"
+        verbose_name_plural = "Facultades"
+
 
 class Usuario(Auditoria):
     user = models.OneToOneField(
@@ -70,11 +84,11 @@ class Usuario(Auditoria):
     )
     rol = models.CharField(
         max_length=20,
-        help_text="Número del documento de identidad del usuario."
+        help_text="Tipo de rol."
     )
     facultad = models.CharField(
         max_length=20,
-        help_text="Número del documento de identidad del usuario."
+        help_text="Facultad a la que pertenece."
     )
 
     def __str__(self):
